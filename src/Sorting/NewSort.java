9c15f7b26e08c9587b0d4c935dece4b1bd20bf06
Asus <Asus@LAPTOP-VIG1E5L7>
2021-02-11 23:35:44 +0530

    NewSort

---
+++ src/Sorting/NewSort.java
@@ -0,0 +1,16 @@
+package Sorting;
+import java.util.Arrays;   
+
+public class NewSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]= {4,2,6};
+		Arrays.sort(arr);
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println(arr[i]);
+		}
+	}
+
+}
